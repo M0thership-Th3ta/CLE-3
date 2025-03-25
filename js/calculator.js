e412b70cccccf3fe531e36549b97dea45db69d5a
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-03-25 14:59:12 +0100

    errors update

--- js/calculator.js
+++ js/calculator.js
@@ -1,21 +1,30 @@
 window.addEventListener("load", init)
 let form;
-let field
+let money
+let error
+let section
+let item
 
 function init() {
+    section = document.querySelector("#formSection")
     form = document.querySelector("#calculatorForm")
-    field = document.querySelector("#to-pay")
+    money = document.querySelector("#to-pay")
+    item = document.querySelector("#to-buy")
+    error = section.querySelector("#error")
     form.addEventListener("submit", formHandler)
 }
 
 function formHandler(e) {
     e.preventDefault()
-    let cash = parseFloat(field.value.replace(",", "."))
+    error.innerText = ""
+    let cash = parseFloat(money.value.replace(",", "."))
     console.log(cash)
-    if (!isNaN(cash)) {
+    if (!isNaN(cash) && cash !== "") {
         console.log("YES!")
+        console.log(item.value)
     } else {
-
+        console.log("error")
+        error.innerText = "U moet een prijs invoeren"
     }
 
 }
