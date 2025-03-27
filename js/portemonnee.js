92e40f64db76ae2b3c32f6697f12e4d2fa19c449
jordi <1102000@hr.nl>
2025-03-27 14:00:57 +0100

    nu echt

--- js/portemonnee.js
+++ js/portemonnee.js
@@ -30,10 +30,10 @@ function verzamelInvoer() {
         {waarde: 5, id: 'biljet5'},
         {waarde: 2, id: 'munt2'},
         {waarde: 1, id: 'munt1'},
-        {waarde: 0.50, id: 'munt50'},
-        {waarde: 0.20, id: 'munt20'},
-        {waarde: 0.10, id: 'munt10'},
-        {waarde: 0.05, id: 'munt5'}
+        {waarde: 0.50, id: 'munt0.50'},
+        {waarde: 0.20, id: 'munt0.20'},
+        {waarde: 0.10, id: 'munt0.10'},
+        {waarde: 0.05, id: 'munt0.05'}
     ].map(item => ({
         waarde: item.waarde,
         aantal: parseInt(document.getElementById(item.id).value) || 0
