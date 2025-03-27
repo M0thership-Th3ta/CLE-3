325715d5a779969f57ee56700270d77870c82c02
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-03-27 12:29:47 +0100

    We got something further?

--- js/calculator.js
+++ js/calculator.js
@@ -10,7 +10,7 @@ function init() {
 
     section = document.querySelector("#my-money")
     getMoney()
-
+    console.log(myMoney)
 
 }
 
@@ -18,15 +18,44 @@ function moneyCount() {
     console.log(myMoney)
 
     for (let i = 0; i < myMoney.length; i++) {
-        console.log(myMoney[i])
         if (myMoney[i] <= price) {
+            let div = document.createElement("div")
+            section.append(div)
+            let p = document.createElement("p")
+            price = price - myMoney[i]
+            p.innerText = myMoney[i]
+            div.append(p)
+            //   myMoney.splice(i, 1)
+            console.log(myMoney)
+
+            if (price <= 0) {
+                console.log(price)
+                return;
+
+            }
+        }
+    }
+
+    for (let i = myMoney.length - 1; i >= 0; i--) {
+        console.log("test")
+
+        if (myMoney[i] > price) {
             let div = document.createElement("div")
             section.append(div)
             let p = document.createElement("p")
             p.innerText = myMoney[i]
             div.append(p)
-            //    portemonnee.splice(i - 1, 1)
             price = price - myMoney[i]
+            //   myMoney.splice(i, 1)
+
+
+            console.log(price)
+
+            if (price <= 0) {
+                console.log(price)
+                return;
+
+            }
         }
     }
 }
