eec15cf25bedb9298f8cca7c98a10c4668e8835e
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-03-27 11:57:18 +0100

    we got something! -2

--- js/calculator.js
+++ js/calculator.js
@@ -2,28 +2,40 @@ window.addEventListener("load", init)
 let portemonnee = JSON.parse(localStorage.getItem('portemonnee')) || []
 let totalSaldo = localStorage.getItem("saldo")
 let price = localStorage.getItem("price")
-let section
+let myMoney = [];
+
+let section;
 
 function init() {
-    section = document.querySelector("#my-money")
-    console.log(totalSaldo)
-    console.log(price)
 
-    moneyCount()
+    section = document.querySelector("#my-money")
+    getMoney()
+
+
 }
 
 function moneyCount() {
-    for (let items of portemonnee) {
-        for (let i = 0; i < items.aantal; i++) {
-            if (items.waarde <= price) {
-                let div = document.createElement("div")
-                section.append(div)
-                let p = document.createElement("p")
-                p.innerText = items.waarde
-                div.append(p)
-                price = price - items.waarde
-                console.log(price)
-            }
+    console.log(myMoney)
+
+    for (let i = 0; i < myMoney.length; i++) {
+        console.log(myMoney[i])
+        if (myMoney[i] <= price) {
+            let div = document.createElement("div")
+            section.append(div)
+            let p = document.createElement("p")
+            p.innerText = myMoney[i]
+            div.append(p)
+            //    portemonnee.splice(i - 1, 1)
+            price = price - myMoney[i]
         }
     }
+}
+
+function getMoney() {
+    for (let contains of portemonnee) {
+        for (let i = 0; i < contains.aantal; i++) {
+            myMoney.push(contains.waarde)
+        }
+    }
+    moneyCount()
 }
