cf6117ab27dfc46141028432a003e4f9a5323a81
anemoia <killboy1100@gmail.com>
2025-03-28 10:02:52 +0100

    Boodschappenlijstje Functionaliteit

--- js/boodschappenlijstje.js
+++ js/boodschappenlijstje.js
@@ -1,20 +1,28 @@
 window.addEventListener("load", init);
 
-//globals
+//globale variabelen
+let inputButton
 let htmlList
 let clearButton
 
-let list = ["teehee", "hoohoo"];
+let list = [];
 
+//Een functie die de initializatie van de pagina regelt
 function init(){
+    //De variabelen initialiseren
+    inputButton = document.querySelector("#input")
     htmlList = document.querySelector("#boodschappenlijst")
     clearButton = document.querySelector("#clear-button")
 
+    //Event listeners
+    inputButton.addEventListener("click", addItems)
     clearButton.addEventListener("click", clearList)
 
+    //De boodschappenlijst weergeven
     listItems()
 }
 
+//Een functie die de boodschappenlijst weergeeft
 function listItems(){
     for (let item of list){
         console.log(item)
@@ -24,6 +32,17 @@ function listItems(){
     }
 }
 
+//Een functie die een item toevoegt aan de boodschappenlijst
+function addItems(){
+    let input = document.querySelector("#boodschap")
+    let li = document.createElement("li")
+    li.textContent = input.value
+    htmlList.appendChild(li)
+    list.push(input.value)
+    input.value = ""
+}
+
+//Een functie die de boodschappenlijst leegt
 function clearList(){
     while(htmlList.hasChildNodes()){
         htmlList.removeChild(htmlList.firstChild)
