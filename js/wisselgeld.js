d3c5feca86569df9d4fd5e6aadda77ae50ad94d2
jordi <1102000@hr.nl>
2025-04-01 13:35:32 +0200

    wisselgeld en link erna toe

---
+++ js/wisselgeld.js
@@ -0,0 +1,15 @@
+// Geldsoorten configuratie
+const geldSoorten = [
+    {waarde: 50, id: 'biljet50'},
+    {waarde: 20, id: 'biljet20'},
+    {waarde: 10, id: 'biljet10'},
+    {waarde: 5, id: 'biljet5'},
+    {waarde: 2, id: 'munt2'},
+    {waarde: 1, id: 'munt1'},
+    {waarde: 0.50, id: 'munt0.50'},
+    {waarde: 0.20, id: 'munt0.20'},
+    {waarde: 0.10, id: 'munt0.10'},
+    {waarde: 0.05, id: 'munt0.05'}
+];
+
+function
